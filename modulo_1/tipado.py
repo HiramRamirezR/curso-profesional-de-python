5465a34b9b95d48f8e336017275a0f26f1f0ec4e
HiramRamirezR <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-09 00:00:12 -0600

    creacion de modulo 1

---
+++ modulo_1/tipado.py
@@ -0,0 +1,16 @@
+# String
+titulo_curso = "Curso profesional de Python."
+
+# Int
+numero = 10
+
+# Float
+numero_decimal = 10.5
+
+# Boolean
+verdadero = True
+
+print(type(titulo_curso))
+print(type(numero))
+print(type(numero_decimal))
+print(type(verdadero))
