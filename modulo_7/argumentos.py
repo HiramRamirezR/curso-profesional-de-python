049f2bbbc68301d0a2581894e99dc7e8f10d5094
HiramRamirezR <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-18 16:42:07 -0600

    Update argumentos.py

--- modulo_7/argumentos.py
+++ modulo_7/argumentos.py
@@ -1,16 +1,31 @@
 # Esta función solamente acepta un argumento
-
 def promedio(listado):
     return sum(listado) / len(listado)
 
 promedios = [10, 8, 6, 9, 7]
 
 print(promedio(promedios))
+# 8.0
+
 
 # Usando *args para aceptar varios argumentos
 # Los argumentos se pasan como una tupla
-
 def promedio_args(*args):
     return sum(args) / len(args)
 
 print(promedio_args(10, 8, 6, 9, 7))
+# 8.0
+
+
+# Haciendo una combinación de argumentos
+def combinacion(p1, p2, *args, p3):
+    print(p1)
+    print(p2)
+    print(args)
+    print(p3)
+
+combinacion("p1", "p2", 1, 2, 3, 4, 5, p3="p3")
+# p1
+# p2
+# (1, 2, 3, 4, 5)
+# p3
