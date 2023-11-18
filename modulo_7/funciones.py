e42136bec883069b4f395be0f66cd4a4230eef98
HiramRamirezR <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-18 01:48:46 -0600

    Update funciones.py

--- modulo_7/funciones.py
+++ modulo_7/funciones.py
@@ -1,7 +1,8 @@
 def suma(numero1, numero2):
-    print(f"La suma de {numero1} y {numero2} es {numero1 + numero2}")
+    return numero1 + numero2
 
 numero1 = int(input("Introduce el primer numero: "))
 numero2 = int(input("Introduce el segundo numero: "))
 
-suma(numero1, numero2)
+resultado = suma(numero1, numero2)
+print(resultado)
