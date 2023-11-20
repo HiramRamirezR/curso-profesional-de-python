182a5e25f7a5c0648524ac6de504440e3bd0a642
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-20 10:01:29 -0600

    Create lambdas.py

---
+++ modulo_7/lambdas.py
@@ -0,0 +1,5 @@
+funcion_grados = lambda grados : grados * 1.8 + 32
+# lambda <parametros> : <expresión>
+# por default la funcion lambda retorna el resultado de la expresión
+
+print(funcion_grados(10))
