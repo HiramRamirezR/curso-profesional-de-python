63b66441ba43bd6b1c36ceb668e347de72850fdb
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-24 09:08:32 -0600

    rearranged repository

--- modulo_7/decoradores.py
+++ modulo_7/decoradores.py
@@ -8,20 +8,18 @@ a(b) -> c
 
 def funcion_a(funcion_b):
 
-    def funcion_c():
+    def funcion_c(*args, **kwargs):
         print(">>> Antes del llamado a la función b")
-        funcion_b()
+        resultado = funcion_b(*args, **kwargs)
         print(">>> Después del llamado a la función b")
+
+        return resultado
 
     return funcion_c
 
 @funcion_a
-def suma():
-    print(1+1)
+def suma(num_1, num_2):
+    return num_1 + num_2
 
-@funcion_a
-def resta():
-    print(5-2)
-
-suma()
-resta()
+resultado = suma(3, 5)
+print(resultado)
