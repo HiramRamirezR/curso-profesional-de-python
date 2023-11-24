36778624408731a37ec866a15557bbaa474c9ada
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2023-11-24 11:51:09 -0600

    Create documentar.py

---
+++ modulo_7/documentar.py
@@ -0,0 +1,16 @@
+# Docstring
+# __doc__ (Módulos, Clases, Métodos y Funciones)
+
+def suma(num_1, num_2):
+    """
+    Funcion que suma dos numeros
+    
+    Parametros:
+    num_1 -> (int)
+    num_2 -> (int)
+
+    Retorna la suma de los dos argumentos.
+    """
+    return num_1 + num_2
+
+print(suma.__doc__)
