48e878a65e440777d6238f26fcc8d9617345677f
HiramRamirezR <68158101+HiramRamirezR@users.noreply.github.com>
2023-12-23 10:44:58 -0600

    Update repository

---
+++ modulo_8_clases/atributos.py
@@ -0,0 +1,12 @@
+# Attrs de clase
+# Attrs de instancia
+
+class Usuario:
+    username = ""
+    email = ""
+
+Usuario.username = "Hiram"
+Usuario.email = "info@gmail.com"
+
+print(Usuario.username)
+print(Usuario.email)
---
+++ modulo_8_clases/clases.py
@@ -0,0 +1,5 @@
+class Usuario:  # El nombre de la clase va en UpperCamelCase y en singular.
+    pass
+
+cody = Usuario()
+print(cody)
